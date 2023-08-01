d51ab4dc78d0275c7eb2d2c1b212908d48111712
Eclipse_ <nightsky5070@gmail.com>
2023-08-01 11:38:38 +0900

    기능 수정중

---
+++ src/main/java/com/springboot/valid_exception/controller/ValidationController.java
@@ -0,0 +1,57 @@
+package com.springboot.valid_exception.controller;
+
+import com.springboot.valid_exception.data.dto.ValidRequestDto;
+import com.springboot.valid_exception.data.group.ValidationGroup1;
+import com.springboot.valid_exception.data.group.ValidationGroup2;
+import jakarta.validation.Valid;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/validation")
+public class ValidationController {
+
+    private final Logger LOGGER = (Logger) LoggerFactory.getLogger(ValidationController.class);
+
+    @PostMapping("/valid")
+    public ResponseEntity<String> checkValidationByValid(
+            @Valid @RequestBody ValidRequestDto validRequestDto) {
+        LOGGER.info(validRequestDto.toString());
+        return ResponseEntity.status(HttpStatus.OK).body(validRequestDto.toString());
+    }
+
+    @PostMapping("/validated")
+    public ResponseEntity<String> checkValidation(
+            @Validated @RequestBody ValidatedRequestDto validatedRequestDto){
+        LOGGER.info(validatedRequestDto.toString());
+        return ResponseEntity.status(HttpStatus.OK).body(validatedRequestDto.toString());
+    }
+
+    @PostMapping("/validated/group1")
+    public ResponseEntity<String> checkValidation1(
+            @Validated(ValidationGroup1.class) @RequestBody ValidatedRequestDto validatedRequestDto) {
+        LOGGER.info(validatedRequestDto.toString());
+        return ResponseEntity.status(HttpStatus.OK).body(validatedRequestDto.toString());
+    }
+    @PostMapping("/validated/group2")
+    public ResponseEntity<String> checkValidation2(
+            @Validated(ValidationGroup2.class) @RequestBody ValidatedRequestDto validatedRequestDto) {
+        LOGGER.info(validatedRequestDto.toString());
+        return ResponseEntity.status(HttpStatus.OK).body(validatedRequestDto.toString());
+    }
+    @PostMapping("/validated/all-group")
+    public ResponseEntity<String> checkValidation3(
+            @Validated({ValidationGroup1.class,
+                    ValidationGroup2.class}) @RequestBody ValidatedRequestDto validatedRequestDto) {
+        LOGGER.info(validatedRequestDto.toString());
+        return ResponseEntity.status(HttpStatus.OK).body(validatedRequestDto.toString());
+    }
+}
